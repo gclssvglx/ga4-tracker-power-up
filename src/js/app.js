8af95366783ac4f892eb1869a6abf31f64f6ea20
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2022-05-28 16:39:10 +0100

    Add bit more

--- src/js/app.js
+++ src/js/app.js
@@ -2,12 +2,11 @@ console.log("Powering up...")
 
 window.TrelloPowerUp.initialize({
   "card-badges": function (t, opts) {
-    return t.card('all')
-    .then(function(card) {
-      console.log(card);
-      return [{
-        text: card.name
-      }];
-    })
+    return t
+      .card("name")
+      .get("name")
+      .then(function (cardName) {
+        console.log("We just loaded the card name for fun: " + cardName);
+      });
   },
 });
